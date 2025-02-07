8c27c719205583cab79a159ca69e33b532c5b413
Luis Roder <luisroder@hotmail.de>
2025-02-07 09:38:22 +0100

    Fixed wrong styllesheet reference on impressum.php

--- impressum.php
+++ impressum.php
@@ -8,7 +8,7 @@
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
     />
-    <link href="styles.css" rel="stylesheet" />
+    <link href="./style/styles.css" rel="stylesheet" />
   </head>
   <body>
     <!-- Navigation -->
